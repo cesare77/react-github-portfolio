7d7531083d1d519cf379e2ff7574696ec15a1412
Cesare Fiamingo <fiamingocesare@yahoo.it>
2025-03-18 18:00:46 +0100

    setup application and create first components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import logo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header logo={logo}/>
+    </div>
+  );
+}
+
+export default App;
